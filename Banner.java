cd609e98931a285bf3d788973a856cb01c9d1d36
Mehreen Mirza <95234760+mmirza21@users.noreply.github.com>
2022-12-21 18:04:32 +0000

    banner edit

--- Banner.java
+++ Banner.java
@@ -8,10 +8,12 @@ public class Banner {
     
     /* Message to print on Banner */
     private String message;
+    private int size;
 
     /* Constructor */
     public Banner(String m) {
-        this.message = "                            HAVE A GREAT DAY!";
+        this.message = m;
+        this.size = m.length();
         
     }
 
@@ -19,34 +21,11 @@ public class Banner {
      * Modify this method to print a decorative banner, resized to fit the message
      */
     public void display() {
-        String message = this.message; 
-        // Unfortunately this code only works to symmetrically reposition the banner if
-        // and only if the length of the message is similar to the length of the asterisks
-        if (message.length()>18 || message.length()<22){
-            for(int i = 0; i < (message.length()/4); i ++){
-                System.out.print(" ");
-            }
-            System.out.println("           ****************************");
-            for(int i = 0; i < (message.length()/4); i ++){
-                System.out.print(" ");
-            }
-            System.out.println("    ********* ~~~~~~~~~~~~~~~~~~~~~~~ ********");
-            System.out.println(message); 
-            for(int i = 0; i < (message.length()/4); i ++){
-                System.out.print(" ");
-            }
-            System.out.println("    ******** ~~~~~~~~~~~~~~~~~~~~~~~~ ********");
-            for(int i = 0; i < (message.length()/4); i ++){
-                System.out.print(" ");
-            }
-            System.out.println("           ****************************");}
-        else{
-            System.out.println("           ****************************");
-            System.out.println("    ********* ~~~~~~~~~~~~~~~~~~~~~~~~ ********");
-            System.out.print(this.message); 
-            System.out.println("    ******** ~~~~~~~~~~~~~~~~~~~~~~~~ ********");
-            System.out.println("           ****************************");
-        }
+        System.out.println("            ****" + "*".repeat(this.size) + "****  ");
+        System.out.println("           *** ~" + "~".repeat(this.size) + "~ *** ");
+        System.out.println("          *** ~ " + this.message + " ~ ***");
+        System.out.println("           *** ~" + "~".repeat(this.size) + "~ *** ");
+        System.out.println("            ****" + "*".repeat(this.size) + "****  ");}
 
     }
 
